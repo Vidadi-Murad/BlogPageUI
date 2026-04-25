308883e9b2218f38085a260f5a0af8a17795541f
VidadiMurad <vidad0murad@gmail.com>
2026-04-25 14:51:58 +0400

    First Commit

---
+++ BlogPageUI/BlogPageUI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BlogPageUI
+//
+//  Created by Vidadi Muradzada on 25.04.26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
